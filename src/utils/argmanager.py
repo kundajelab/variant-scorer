20dd9ed86c9fd8e398f6ca2bb0bdd3f3f190e460
soumyakundu <soumya.kundu@stanford.edu>
2022-05-02 21:44:46 -0700

    Updated batch size

--- src/utils/argmanager.py
+++ src/utils/argmanager.py
@@ -9,7 +9,7 @@ def update_scoring_args(parser):
     parser.add_argument("-b", "--bias", type=str, help="Bias model to use for variant scoring")
     parser.add_argument("-li", "--lite", action='store_true', help="Models were trained with chrombpnet-lite")
     parser.add_argument("-dm", "--debug_mode", action='store_true', help="Display allele input sequences")
-    parser.add_argument("-bs", "--batch_size", type=int, default=2048, help="Batch size to use for the model")
+    parser.add_argument("-bs", "--batch_size", type=int, default=512, help="Batch size to use for the model")
     parser.add_argument("-sc", "--schema", type=str, choices=['bed', 'plink'], default='bed', help="Format for the input variants list")
     
 def fetch_scoring_args():
