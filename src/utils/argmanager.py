630fe0949a276a7f6b7752296bd104c45c2f4ba2
soumyakundu <soumya.kundu@stanford.edu>
2022-05-23 22:00:09 -0700

    Made peak chrom sizes not required

--- src/utils/argmanager.py
+++ src/utils/argmanager.py
@@ -7,7 +7,7 @@ def update_scoring_args(parser):
     parser.add_argument("-m", "--model", type=str, required=True, help="ChromBPNet model to use for variant scoring")
     parser.add_argument("-o", "--out_prefix", type=str, required=True, help="Path to storing snp effect score predictions from the script, directory should already exist")
     parser.add_argument("-s", "--chrom_sizes", type=str, required=True, help="Path to TSV file with chromosome sizes")
-    parser.add_argument("-ps", "--peak_chrom_sizes", type=str, required=True, help="Path to TSV file with chromosome sizes for peak genome")
+    parser.add_argument("-ps", "--peak_chrom_sizes", type=str, help="Path to TSV file with chromosome sizes for peak genome")
     parser.add_argument("-b", "--bias", type=str, help="Bias model to use for variant scoring")
     parser.add_argument("-li", "--lite", action='store_true', help="Models were trained with chrombpnet-lite")
     parser.add_argument("-dm", "--debug_mode", action='store_true', help="Display allele input sequences")
