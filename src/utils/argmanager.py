5c553b86774526371beee1a5a8377f796939d5ba
soumyakundu <soumya.kundu@stanford.edu>
2022-05-24 04:36:46 +0000

    Adding latest version

--- src/utils/argmanager.py
+++ src/utils/argmanager.py
@@ -2,19 +2,23 @@ import argparse
 
 def update_scoring_args(parser):
     parser.add_argument("-l", "--list", type=str, required=True, help="TSV file containing list of variants to score")
-    parser.add_argument("-g", "--genome", type=str, required=True, help="Genome fasta")    
+    parser.add_argument("-g", "--genome", type=str, required=True, help="Genome fasta")
+    parser.add_argument("-pg", "--peak_genome", type=str, help="Genome fasta for peaks")
     parser.add_argument("-m", "--model", type=str, required=True, help="ChromBPNet model to use for variant scoring")
     parser.add_argument("-o", "--out_prefix", type=str, required=True, help="Path to storing snp effect score predictions from the script, directory should already exist")
     parser.add_argument("-s", "--chrom_sizes", type=str, required=True, help="Path to TSV file with chromosome sizes")
+    parser.add_argument("-ps", "--peak_chrom_sizes", type=str, required=True, help="Path to TSV file with chromosome sizes for peak genome")
     parser.add_argument("-b", "--bias", type=str, help="Bias model to use for variant scoring")
     parser.add_argument("-li", "--lite", action='store_true', help="Models were trained with chrombpnet-lite")
     parser.add_argument("-dm", "--debug_mode", action='store_true', help="Display allele input sequences")
     parser.add_argument("-bs", "--batch_size", type=int, default=512, help="Batch size to use for the model")
     parser.add_argument("-sc", "--schema", type=str, choices=['bed', 'plink'], default='bed', help="Format for the input variants list")
     parser.add_argument("-p", "--peaks", type=str, help="Bed file containing peak regions")
-    
+
 def fetch_scoring_args():
     parser = argparse.ArgumentParser()
     update_scoring_args(parser)
     args = parser.parse_args()
+    print(args)
     return args
+
