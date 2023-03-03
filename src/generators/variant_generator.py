d19246c21a9107b03987d46a5c829742ec2063a5
ziwei-75 <ziwei75@stanford.edu>
2023-03-03 20:38:35 +0000

    always check allele

--- src/generators/variant_generator.py
+++ src/generators/variant_generator.py
@@ -36,6 +36,8 @@ class VariantGenerator(Sequence):
             allele2 = ""
         ### 1 - indexed position 
         pos = pos - 1
+        
+        assert self.genome[chrom][pos:pos+len(allele1)] == allele1
         if len(allele1) == 1 and len(allele2) == 1:
             flank  = str(self.genome[chrom][pos-self.flank_size:pos+self.flank_size])
             if self.shuf:
@@ -46,7 +48,6 @@ class VariantGenerator(Sequence):
 
         ### handle INDELS (allele1 must be the reference allele)
         else:
-            assert self.genome[chrom][pos:pos+len(allele1)] == allele1
             mismatch_length = len(allele1) - len(allele2)
             if mismatch_length > 0:
                 flank = str(self.genome[chrom][pos-self.flank_size:pos+self.flank_size+mismatch_length])
