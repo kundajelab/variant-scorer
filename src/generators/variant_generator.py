5e027d548989a7007b1ae51a78873c4ff77976b4
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2023-05-08 15:40:24 -0700

    wip

--- src/generators/variant_generator.py
+++ src/generators/variant_generator.py
@@ -5,16 +5,19 @@ import math
 import pyfaidx
 from utils import one_hot
 from deeplift.dinuc_shuffle import dinuc_shuffle
+import warnings
 
 class VariantGenerator(Sequence):
-    def __init__(self,
-                 variants_table,
-                 input_len,
-                 genome_fasta,
-                 batch_size=512,
-                 debug_mode=False,
-                 shuf=False):
-
+    def __init__(
+        self,
+        variants_table,
+        input_len,
+        genome_fasta,
+        batch_size=512,
+        debug_mode=False,
+        shuf=False,
+        return_coords=False,
+    ):
         self.variants_table = variants_table
         self.num_variants = self.variants_table.shape[0]
         self.input_len = input_len
@@ -23,73 +26,151 @@ class VariantGenerator(Sequence):
         self.flank_size = self.input_len // 2
         self.shuf = shuf
         self.batch_size = batch_size
+        self.return_coords = return_coords
 
     def __get_allele_seq__(self, chrom, pos, allele1, allele2, seed=-1):
         chrom = str(chrom)
         pos = int(pos)
         allele1 = str(allele1)
         allele2 = str(allele2)
-        
+
         if allele1 == "-":
             allele1 = ""
         if allele2 == "-":
             allele2 = ""
-        ### 1 - indexed position 
+        ### 1 - indexed position
         pos = pos - 1
-        
+
         if len(allele1) == len(allele2):
-            flank  = str(self.genome[chrom][pos-self.flank_size:pos+self.flank_size])
+            flank = str(
+                self.genome[chrom][pos - self.flank_size : pos + self.flank_size]
+            )
             if self.shuf:
                 assert seed != -1
                 flank = dinuc_shuffle(flank, rng=np.random.RandomState(seed))
-            allele1_seq = flank[:self.flank_size] + allele1 + flank[self.flank_size+len(allele1):]
-            allele2_seq = flank[:self.flank_size] + allele2 + flank[self.flank_size+len(allele2):]
+            allele1_seq = (
+                flank[: self.flank_size]
+                + allele1
+                + flank[self.flank_size + len(allele1) :]
+            )
+            allele2_seq = (
+                flank[: self.flank_size]
+                + allele2
+                + flank[self.flank_size + len(allele2) :]
+            )
 
         ### handle INDELS (allele1 must be the reference allele)
         else:
             ### hg19 has lower case
             assert len(allele1) != len(allele2)
-            assert self.genome[chrom][pos:pos+len(allele1)].seq.upper() == allele1
+            assert self.genome[chrom][pos : pos + len(allele1)].seq.upper() == allele1
             mismatch_length = len(allele1) - len(allele2)
-            if mismatch_length > 0:
-                flank = str(self.genome[chrom][pos-self.flank_size:pos+self.flank_size+mismatch_length])
-            else:
-                flank = str(self.genome[chrom][pos-self.flank_size:pos+self.flank_size])
+            if mismatch_length > 0:  # deletion
+                flank = str(
+                    self.genome[chrom][
+                        pos - self.flank_size : pos + self.flank_size + mismatch_length
+                    ]
+                )
+            else:  # insertion
+                flank = str(
+                    self.genome[chrom][pos - self.flank_size : pos + self.flank_size]
+                )
 
             if self.shuf:
                 assert seed != -1
                 flank = dinuc_shuffle(flank, rng=np.random.RandomState(seed))
 
-            left_flank=flank[:self.flank_size]
+            left_flank = flank[: self.flank_size]
 
-            allele1_right_flank = flank[self.flank_size+len(allele1):self.flank_size*2]
-            allele2_right_flank = flank[self.flank_size+len(allele1):self.flank_size*2+mismatch_length]
-            
+            allele1_right_flank = flank[
+                self.flank_size + len(allele1) : self.flank_size * 2
+            ]
+            allele2_right_flank = flank[
+                self.flank_size + len(allele1) : self.flank_size * 2 + mismatch_length
+            ]
+
             allele1_seq = left_flank + allele1 + allele1_right_flank
             allele2_seq = left_flank + allele2 + allele2_right_flank
 
         assert len(allele1_seq) == self.flank_size * 2
         assert len(allele2_seq) == self.flank_size * 2
-        return allele1_seq, allele2_seq
+
+        allele1_coords, allele2_coords = [chrom, -1, -1], [chrom, -1, -1]
+        # we don't support coords for indel variants
+        if self.return_coords and (len(allele1) == len(allele2)):
+            allele1_coords = [chrom, pos - self.flank_size, pos + self.flank_size]
+            allele2_coords = allele1_coords
+            # validate
+            assert allele1_coords[2] - allele1_coords[1] == len(allele1_seq)
+            assert allele2_coords[2] - allele2_coords[1] == len(allele2_seq)
+            seq = str(self.genome[chrom][allele1_coords[1] : allele1_coords[2]])
+            # seq could equal allele1_seq or allele2_seq depending on which is the reference allele
+            # or it could equal neither, if the variants stem from a study where neither allele is a reference allele
+            # in the latter case, emit a Warning, which could be suppressed if desired
+            if (seq != allele1_seq) and (seq != allele2_seq):
+                msg = "Neither allele seems to be a reference allele.\n" \
+                    f"chrom: {chrom}, pos: {pos}, allele1: {allele1}, allele2: {allele2}"
+                warnings.warn(msg)
+
+        return allele1_seq, allele2_seq, allele1_coords, allele2_coords
 
     def __getitem__(self, idx):
-        cur_entries = self.variants_table.iloc[idx*self.batch_size:min([self.num_variants,(idx+1)*self.batch_size])]
-        rsids = cur_entries['rsid'].tolist()
+        cur_entries = self.variants_table.iloc[
+            idx
+            * self.batch_size : min([self.num_variants, (idx + 1) * self.batch_size])
+        ]
+        rsids = cur_entries["rsid"].tolist()
 
         if self.shuf:
-            allele1_seqs, allele2_seqs = zip(*[self.__get_allele_seq__(v, w, x, y, z) for v,w,x,y,z in
-                                             zip(cur_entries.chr, cur_entries.pos,
-                                                 cur_entries.allele1, cur_entries.allele2, cur_entries.random_seed)])
+            allele1_seqs, allele2_seqs, allele1_coords, allele2_coords = zip(
+                *[
+                    self.__get_allele_seq__(v, w, x, y, z)
+                    for v, w, x, y, z in zip(
+                        cur_entries.chr,
+                        cur_entries.pos,
+                        cur_entries.allele1,
+                        cur_entries.allele2,
+                        cur_entries.random_seed,
+                    )
+                ]
+            )
         else:
-            allele1_seqs, allele2_seqs = zip(*[self.__get_allele_seq__(w, x, y, z) for w,x,y,z in
-                                             zip(cur_entries.chr, cur_entries.pos, cur_entries.allele1, cur_entries.allele2)])
+            allele1_seqs, allele2_seqs, allele1_coords, allele2_coords = zip(
+                *[
+                    self.__get_allele_seq__(w, x, y, z)
+                    for w, x, y, z in zip(
+                        cur_entries.chr,
+                        cur_entries.pos,
+                        cur_entries.allele1,
+                        cur_entries.allele2,
+                    )
+                ]
+            )
 
-        if self.debug_mode:
-            return rsids, list(allele1_seqs),list(allele2_seqs)
-        else:
-            return rsids, one_hot.dna_to_one_hot(list(allele1_seqs)), one_hot.dna_to_one_hot(list(allele2_seqs))
-        
+        allele1_seqs_ret = (
+            list(allele1_seqs)
+            if self.debug_mode
+            else one_hot.dna_to_one_hot(list(allele1_seqs))
+        )
+        allele2_seqs_ret = (
+            list(allele2_seqs)
+            if self.debug_mode
+            else one_hot.dna_to_one_hot(list(allele2_seqs))
+        )
+
+        if not self.return_coords:
+            return (
+                rsids,
+                allele1_seqs_ret,
+                allele2_seqs_ret,
+            )
+        return (
+            rsids,
+            allele1_seqs_ret,
+            allele2_seqs_ret,
+            list(allele1_coords),
+            list(allele2_coords),
+        )
 
     def __len__(self):
-        return math.ceil(self.num_variants/self.batch_size)
-
+        return math.ceil(self.num_variants / self.batch_size)
---
+++ src/variants_to_regions.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import argparse
+from generators.variant_generator import VariantGenerator
+from utils.helpers import load_variant_table, get_valid_variants
+from pathlib import Path
+import warnings
+
+def fetch_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--list",
+        type=str,
+        required=True,
+        help="TSV file containing the list of variants",
+    )
+    parser.add_argument(
+        "-sc",
+        "--schema",
+        type=str,
+        choices=["bed", "plink", "neuro-variants", "chrombpnet", "original"],
+        default="chrombpnet",
+        help="Format for the input variants list",
+    )
+    parser.add_argument("-g", "--genome", type=str, required=True, help="Genome fasta")
+    parser.add_argument(
+        "-s",
+        "--chrom_sizes",
+        type=str,
+        required=True,
+        help="Path to TSV file with chromosome sizes",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Path to store shap summary h5 files, \
+         directory should already exist",
+    )
+    parser.add_argument(
+        "-il",
+        "--input_len",
+        type=int,
+        default=2114,
+        help="Length of the input sequence (in bp)",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = fetch_args()
+    print(args)
+
+    if not Path(args.output_dir).exists():
+        raise ValueError("Output directory does not exist")
+
+    chrom_sizes = pd.read_csv(
+        args.chrom_sizes, header=None, sep="\t", names=["chrom", "size"]
+    )
+    chrom_sizes_dict = chrom_sizes.set_index("chrom")["size"].to_dict()
+
+    variants_table = load_variant_table(args.list, args.schema)
+    variants_table = variants_table.fillna("-")
+    print(f"Variants table shape before validation: {variants_table.shape}")
+    variants_table = variants_table.loc[
+        variants_table.apply(
+            lambda x: get_valid_variants(
+                x.chr, x.pos, x.allele1, x.allele2, args.input_len, chrom_sizes_dict
+            ),
+            axis=1,
+        )
+    ]
+    variants_table.reset_index(drop=True, inplace=True)
+    print(f"Variants table shape after validation: {variants_table.shape}")
+
+    var_gen = VariantGenerator(
+        variants_table=variants_table,
+        input_len=args.input_len,
+        genome_fasta=args.genome,
+        batch_size=variants_table.shape[0],
+        return_coords=True,
+    )
+
+    assert len(var_gen) == 1
+    _, _, _, allele1_coords, allele2_coords = var_gen[0]
+    # add empty columns and the last column
+    empty_cols = ["."] * 6
+    allele1_coords_all = [x + empty_cols + [args.input_len//2] for x in allele1_coords]
+    allele2_coords_all = [x + empty_cols + [args.input_len//2] for x in allele2_coords]
+    # make dataframes
+    df1 = pd.DataFrame(allele1_coords_all)
+    df2 = pd.DataFrame(allele2_coords_all)
+
+    # filter out any indel variants (i.e. those with -1 in the position columns)
+    # first detect any such variants and raise a warning
+    if -1 in df1[1].to_list():
+        msg = "It appears that you have indel variants. We don't support mapping these to regions " \
+            "in the reference genome. Thus these won't be present in the resulting bed files."
+        warnings.warn(msg)
+    df1 = df1[df1[1] != -1]
+    df2 = df2[df2[1] != -1]
+
+    # write to bed files
+    df1.to_csv(Path(args.output_dir) / "allele1_variants_regions.bed", sep="\t", header=None, index=None)
+    df2.to_csv(Path(args.output_dir) / "allele2_variants_regions.bed", sep="\t", header=None, index=None)
+
+if __name__ == "__main__":
+    main()
