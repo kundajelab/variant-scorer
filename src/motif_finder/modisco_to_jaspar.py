a868e0d288a0da40081e08d1a106637215acf868
soumyakundu <soumya.kundu@stanford.edu>
2023-05-23 14:52:47 +0000

    Added modisco to jaspar script

--- src/motif_finder/modisco_to_jaspar.py
+++ src/motif_finder/modisco_to_jaspar.py
@@ -16,25 +16,6 @@ import pandas as pd
 
 #     return ppm[max(maxes[0][0]-flank, 0):maxes[0][-1]+1+flank]
 
-def write_prelim_lines(out_file):
-    out_file.write("MEME version 4\n")
-    out_file.write("\n")
-    out_file.write("ALPHABET= ACGT\n")
-    out_file.write("\n")
-    out_file.write("strands: + -\n")
-    out_file.write("\n")
-    out_file.write("Background letter frequencies\n")
-    out_file.write("A 0.25 C 0.25 G 0.25 T 0.25\n")
-    out_file.write("\n")
-
-def write_for_one_motif(pfm, motif, out_file):
-    out_file.write("MOTIF " + motif + "\n")
-    out_file.write("letter-probability matrix: alength= 4 w= %i nsites= 200\n"%(len(pfm)))
-    for row in pfm:
-        out_file.write(' '.join([str(x) for x in row]) + "\n")
-    out_file.write("URL none\n")
-    out_file.write("\n")
-
 def trim_motif(ppm, cwm, background=[0.25, 0.25, 0.25, 0.25],trim_threshold=0.3):
     score = np.sum(np.abs(cwm), axis=1)
     trim_thresh = np.max(score) * trim_threshold  # Cut off anything less than 30% of max score
@@ -50,7 +31,7 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--modisco_h5py", required=True, type=str)
     parser.add_argument("-mt", "--modisco_tomtom", required=True, type=str)
-    parser.add_argument("-o", "--output_prefix", required=True, type=str)
+    parser.add_argument("-o", "--output_dir", required=True, type=str)
     parser.add_argument("-t", "--threshold", default=0.3, type=float)
     parser.add_argument("-ml", "--min_length", default=3, type=int, help="Min length of acceptable motif")
     parser.add_argument("-f", "--flank_add", default=0, type=int, help="Add this on either side of motif after trimming")
@@ -67,12 +48,6 @@ if __name__=='__main__':
         if name not in modisco_results.keys():
             continue
 
-        if name == 'pos_patterns':
-            output_file = open(args.output_prefix + '.pos.meme.txt', "w")
-        else:
-            output_file = open(args.output_prefix + '.neg.meme.txt', "w")
-
-        write_prelim_lines(output_file)
         metacluster = modisco_results[name]
         key = lambda x: int(x[0].split("_")[-1])
         for pattern_name, pattern in sorted(metacluster.items(), key=key):
@@ -85,7 +60,7 @@ if __name__=='__main__':
                 if num_seqlets >= args.min_seqlets:
                     curr_motif = tomtom_results.at[name + "." + pattern_name, "best_matches"]
                     curr_motif = "NoMatch--" + name + "." + pattern_name if (type(curr_motif) == float and np.isnan(curr_motif)) else curr_motif
-                    write_for_one_motif(trimmed, curr_motif, output_file)
+                    np.savetxt(args.output_dir + '/' + curr_motif + '.ppm.txt', trimmed.transpose())
 
     modisco_results.close()
 
