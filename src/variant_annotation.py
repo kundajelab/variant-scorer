969a41a7cac94c6e88ed0233217251385f2ec80b
bytewife <ivy.ember.raine@gmail.com>
2024-04-01 12:46:33 -0700

    Implement `r2` flag in `annotation` subcommand

--- src/variant_annotation.py
+++ src/variant_annotation.py
@@ -98,6 +98,55 @@ def main(args = None):
 
         variant_scores['peak_overlap'] = variant_scores['variant_id'].isin(peak_intersect_df[5].tolist())
 
+    if args.r2:
+        r2_ld_filepath = args.r2
+
+        r2_tsv_filepath = "/tmp/r2.tsv"
+        with open(r2_ld_filepath, 'r') as r2_ld_file, open(r2_tsv_filepath, mode='w') as r2_tsv_file:
+            # temp=r2_tsv_file.name
+            for line in r2_ld_file:
+                # Process the line
+                line = '\t'.join(line.split())
+                # Write the processed line to the output file, no need to specify end='' as '\n' is added explicitly
+                r2_tsv_file.write(line + '\n')
+            r2_tsv_file.flush()
+            
+        with open(r2_tsv_filepath, 'r') as r2_tsv_file:
+            plink_variants = pd.read_table(r2_tsv_file)
+            print("plink_variants:")
+            print(plink_variants.head())
+            print(plink_variants.shape)
+
+            # Get just the lead variants, which is provided by the user.
+            lead_variants = variant_scores[['chr', 'pos', 'variant_id']].copy()
+            lead_variants['r2'] = 1.0
+            lead_variants['lead_variant'] = lead_variants['variant_id']
+            print("lead_variants:")
+            print(lead_variants.head())
+            print(lead_variants.shape)
+
+            # Get just the ld variants.
+            plink_ld_variants = plink_variants[['SNP_A','CHR_B','BP_B','SNP_B','R2']].copy()
+            plink_ld_variants.columns = ['lead_variant', 'chr', 'pos', 'variant_id', 'r2']
+            plink_ld_variants = plink_ld_variants[['chr', 'pos', 'variant_id', 'r2', 'lead_variant']]
+            plink_ld_variants['chr'] = 'chr' + plink_ld_variants['chr'].astype(str)
+            plink_ld_variants = plink_ld_variants.sort_values(by=['variant_id', 'r2'], ascending=False).drop_duplicates(subset='variant_id')
+            print("plink_ld_variants:")
+            print(plink_ld_variants.head())
+            print(plink_ld_variants.shape)
+
+            all_plink_variants = pd.concat([lead_variants, plink_ld_variants])
+            all_plink_variants = all_plink_variants[['variant_id', 'r2', 'lead_variant']]
+            all_plink_variants = all_plink_variants.sort_values( by=['variant_id', 'r2'], ascending=False)
+            print("all_plink_variants:")
+            print(all_plink_variants.head())
+            print(all_plink_variants.shape)
+
+            variant_scores = variant_scores.merge(all_plink_variants,
+                on=['variant_id'],
+                how='left')
+
+
     os.remove(tmp_bed_file_path)
 
     print()
@@ -106,9 +155,11 @@ def main(args = None):
     print()
 
     out_file = output_prefix + ".annotations.tsv"
+
     variant_scores.to_csv(out_file,\
                           sep="\t",\
                           index=False)
+    print("Output written to:", out_file)
 
     print("DONE")
 
