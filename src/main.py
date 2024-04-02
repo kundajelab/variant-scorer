3af9648d2ffcb3de8be3090f37542c6eac4ae07e
bytewife <ivy.ember.raine@gmail.com>
2024-04-02 11:59:55 -0700

    Disable scoring-per-chrom & scoring-per-chunk

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils import argmanager
 import variant_scoring
-import variant_scoring_per_chrom
-import variant_scoring_per_chunk
+# import variant_scoring_per_chrom
+# import variant_scoring_per_chunk
 import variant_shap
 import variant_summary_across_folds
 import variant_annotation
@@ -11,10 +11,10 @@ def cli():
 	args = argmanager.fetch_main_parser()
 	if args.subcommand == "score":
 		variant_scoring.main(args)
-	elif args.subcommand == "score-per-chrom":
-		variant_scoring_per_chrom.main(args)
-	elif args.subcommand == "score-per-chunk":
-		variant_scoring_per_chunk.main(args)
+	# elif args.subcommand == "score-per-chrom":
+	# 	variant_scoring_per_chrom.main(args)
+	# elif args.subcommand == "score-per-chunk":
+	# 	variant_scoring_per_chunk.main(args)
 	elif args.subcommand == "shap":
 		variant_shap.main(args)
 	elif args.subcommand == "summary":
